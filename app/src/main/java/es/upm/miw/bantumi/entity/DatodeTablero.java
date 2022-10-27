21d8cdad7ccd7961568bfece5bad3d85e9976aef
1257198442 <1257198442@qq.com>
2022-10-27 21:07:51 +0200

    add the ability to save game data #4

---
+++ app/src/main/java/es/upm/miw/bantumi/entity/DatodeTablero.java
@@ -0,0 +1,11 @@
+package es.upm.miw.bantumi.entity;
+
+public class DatodeTablero {
+    public int numero_de_piezas;
+    public int ubicacion;
+
+    public DatodeTablero(int numero_de_piezas, int ubicacion) {
+        this.numero_de_piezas = numero_de_piezas;
+        this.ubicacion = ubicacion;
+    }
+}
